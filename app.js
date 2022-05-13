8f14dace616c4f6099423c351df77a487def0528
Gustavo Benitez <benitezlgustavo@gmail.com>
2022-05-13 16:43:09 -0500

    Filtrar Grupos

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const Pool = require("pg").Pool;
 require('dotenv').config()
 
+const ejs = require("ejs");
+ejs.delimiter = '/';
+ejs.openDelimiter = '[';
+ejs.closeDelimiter = ']';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -29,18 +34,31 @@ app.get('/', async (req, res) => {
   }
 });
 
+app.get('/formulario', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query(`select "Proyecto" from "Proyectos"`);
+    res.render('pages/form.ejs', { proyectos: result.rows } );
+    client.release();
+} catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+}
+});
+
 app.get('/grupos', async (req, res) => {
     try {
         const client = await pool.connect();
-        const result = await client.query(
-        `select "Beneficiarios"."id_beneficiario", "Beneficiarios"."Nombre", 
-        "Beneficiarios"."Fecha de Nacimiento", "Grupos"."Grupo", "Grupos"."Sede"
-        from "Beneficiarios"
-        inner join "Beneficiarios_Grupos"
-        on "Beneficiarios".id_beneficiario = "Beneficiarios_Grupos".id_beneficiario
-        inner join "Grupos"
-        on "Grupos".id_grupo = "Beneficiarios_Grupos".id_grupo`);
-        res.render('pages/grupos.ejs', { beneficiarios: result.rows } );
+        const grupos = await client.query(`select * from "Grupos"`);
+        const beneficiarios = await client.query(
+          `select b."id_beneficiario", b."Nombre", b."Fecha de Nacimiento", g."Grupo"
+            from "Beneficiarios" b
+            inner join "Beneficiarios_Grupos" bg
+            on b."id_beneficiario" = bg."id_beneficiario"
+            inner join "Grupos" g
+            on bg."id_grupo" = g."id_grupo"`);
+        res.render('pages/grupos.ejs', 
+          { grupos: grupos.rows, beneficiarios: beneficiarios.rows } );
         client.release();
     } catch (err) {
         console.error(err);
